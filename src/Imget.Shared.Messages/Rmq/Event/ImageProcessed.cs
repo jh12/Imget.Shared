8c46b55b2713655e5b3621f6863310ff45048ad2
jh12 <jabaha35@hotmail.com>
2025-01-26 15:07:40 +0100

    Add ImageProcessed message

---
+++ src/Imget.Shared.Messages/Rmq/Event/ImageProcessed.cs
@@ -0,0 +1,10 @@
+﻿namespace Imget.Shared.Messages.Rmq.Event;
+
+public record ImageProcessed
+(
+    string Title,
+    string Description,
+    string[] Tags,
+    Uri ThumbnailPath,
+    Uri FullPath
+);
--- src/Imget.Shared.Messages/Rmq/RmqMessageGenerationContext.cs
+++ src/Imget.Shared.Messages/Rmq/RmqMessageGenerationContext.cs
@@ -1,12 +1,18 @@
 ﻿using System.Text.Json.Serialization;
+using Imget.Shared.Messages.Rmq.Event;
 using Imget.Shared.Messages.Rmq.Request;
 using Imget.Shared.Messages.Rmq.Response;
 
 namespace Imget.Shared.Messages.Rmq;
 
+// Events
+[JsonSerializable(typeof(ImageProcessed))]
+
+// Requests
 [JsonSerializable(typeof(RequestBase))]
 [JsonSerializable(typeof(TaggingRequest))]
 
+// Responses
 [JsonSerializable(typeof(ResponseBase))]
 [JsonSerializable(typeof(TaggingResponse))]
 public partial class RmqMessageGenerationContext : JsonSerializerContext
